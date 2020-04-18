d221cdb1eb845ee1e3d7ec6f2e579e6fb97583e3
Khem Sok <khemsok97@gmail.com>
2020-04-18 13:43:23 -0400

    Fixed search box so that it disappears when user erase text

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -16,13 +16,15 @@ function Homepage({ history }) {
   const [movieOptions, setMovieOptions] = useState([]);
 
   const fetchMovieOptions = async (title) => {
-    if (title.length >= 3) {
+    if (title.length >= 2) {
       const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=67dd87ea202627ab42b62941829e7dec&language=en-US&query=${title}&page=1&include_adult=false`;
 
       const response = await fetch(searchUrl);
       const data = await response.json();
 
       setMovieOptions(data["results"]);
+    } else {
+      setMovieOptions([]);
     }
   };
 
